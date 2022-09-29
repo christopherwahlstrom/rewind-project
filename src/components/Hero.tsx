a7d64783afadf4a77255cf863f3fe56a4ac7c545
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-09-29 12:13:14 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,16 @@
 import './hero.scss'
+import PlayerForm from './PlayerForm';
 import { useNavigate } from 'react-router-dom'
 
 const Hero = () => {
+    const navigate = useNavigate();
+
 
     return (
         <section className='main-hero'>
             <article>
-                
+                <PlayerForm />
             </article>
-
-
         </section>
     )
 }
